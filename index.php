17761ca319449f6cb2f52ae1a19fdf9464f2b6bb
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-18 18:22:53 +0300

    Add title to HTML head for Student Management System

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once 'vendor/autoload.php';
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>SMS</title>
+    <title>Student-Management-System</title>
     <!--    Font Awesome Link-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
           integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
