6c7d0469866bc5f0f5eac1446f9b6846cf546164
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-20 02:09:05 +0300

    Remove unused HTML structure and enhance navigation bar with branding and login button.

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,13 +1,13 @@
-<div class="">
+<div class="container">
     <nav class="navbar navbar-expand-lg navbar-light ">
         <div class="container-fluid">
-            <a href="#" class="navbar-brand">Brand</a>
+            <a href="/" class="navbar-brand">Schools</a>
             <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <div class="navbar-nav w-100 ">
-                    <a href="#" class="nav-item nav-link active">Home</a>
+                    <a href="/" class="nav-item nav-link active">Home</a>
                     <a href="#" class="nav-item nav-link">Profile</a>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Messages</a>
@@ -20,7 +20,7 @@
                     <a href="#" class="nav-item nav-link disabled" tabindex="-1">Reports</a>
                 </div>
                 <div class="navbar-nav ms-auto">
-                    <a href="#" class="nav-item nav-link">Login</a>
+                   <button type="button" class=" nav-item btn btn-outline-primary mb-2">Login</button>
                 </div>
             </div>
         </div>
