86d1b250aa2dd22dcc25d821b2772edbd6c274e9
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-21 14:40:21 +0300

    create back to top button

---
+++ views/partials/Back_to_top_button.php
@@ -0,0 +1,8 @@
+<!-- Back to top button -->
+<button
+        type="button"
+        class="btn btn-danger btn-floating btn-lg"
+        id="btn-back-to-top"
+>
+    <i class="fas fa-arrow-up"></i>
+</button>
