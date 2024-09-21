4088d95b6578127e8b2b0f51110d437fa5fee62e
abdelrhman <abdelrhmanroshdy8@gmail.com>
2024-09-21 18:40:59 +0300

    Implement array utility class with various methods for manipulation and access.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Dotenv\Dotenv;
+use PROJECT\support\Arr;
 
 require_once '../src/support/helpers.php';
 require_once base_path() . 'vendor/autoload.php';
@@ -8,4 +9,3 @@ require_once base_path() . 'routes/web.php';
 $env = Dotenv::createImmutable(base_path());
 $env->load();
 app()->run();
-
